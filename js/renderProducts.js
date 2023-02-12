360087928dee63010ae5158b95f2e68388e91948
dmitriy-lesnick <120184471+dmitriy-lesnick@users.noreply.github.com>
2023-02-12 13:50:59 +0300

    Add files via upload

--- js/renderProducts.js
+++ js/renderProducts.js
@@ -1,53 +1,50 @@
 const productsContainer = document.querySelector('#products-container');
 
+// Запускаем getProducts
 getProducts();
 
+// Асинхронная функция получения данных из файла products.json
 async function getProducts() {
-
+	// Получаем данные из products.json
     const response = await fetch('./js/products.json');
-
+    // Парсим данные из JSON формата в JS
     const productsArray = await response.json();
-
-    renderProducts(productsArray);
+    // Запускаем ф-ю рендера (отображения товаров)
+	renderProducts(productsArray);
 }
-
-
-getProducts();
 
 function renderProducts(productsArray) {
-
-    productsArray.forEach(item => {
+    productsArray.forEach(function (item) {
         const productHTML = `<div class="col-md-6">
-        <div class="card mb-4" data-id="${item.id}">
-            <img class="product-img" src="img/roll/${item.imgSrc}" alt="">
-            <div class="card-body text-center">
-                <h4 class="item-title">${item.title}</h4>
-                <p><small data-items-in-box class="text-muted">${item.itemsInBox}</small></p>
+						<div class="card mb-4" data-id="${item.id}">
+							<img class="product-img" src="img/roll/${item.imgSrc}" alt="">
+							<div class="card-body text-center">
+								<h4 class="item-title">${item.title}</h4>
+								<p><small data-items-in-box class="text-muted">${item.itemsInBox} шт.</small></p>
 
-                <div class="details-wrapper">
-                    <div class="items counter-wrapper">
-                        <div class="items__control" data-action="minus">-</div>
-                        <div class="items__current" data-counter>1</div>
-                        <div class="items__control" data-action="plus">+</div>
-                    </div>
+								<div class="details-wrapper">
 
-                    <div class="price">
-                        <div class="price__weight">${item.weight} г.</div>
-                        <div class="price__currency">${item.price} ₽</div>
-                    </div>
-                </div>
+									<!-- Счетчик -->
+									<div class="items counter-wrapper">
+										<div class="items__control" data-action="minus">-</div>
+										<div class="items__current" data-counter>1</div>
+										<div class="items__control" data-action="plus">+</div>
+									</div>
+									<!-- // Счетчик -->
 
-                <button data-cart type="button" class="btn btn-block btn-outline-warning">+ в
-                    корзину</button>
+									<div class="price">
+										<div class="price__weight">${item.weight}г.</div>
+										<div class="price__currency">${item.price} ₽</div>
+									</div>
+								</div>
 
-            </div>
-        </div>
-    </div>`;
+								<button data-cart type="button" class="btn btn-block btn-outline-warning">
+									+ в корзину
+								</button>
 
-        productsContainer.insertAdjacentElement('beforeend', productHTML)
-
+							</div>
+						</div>
+					</div>`;
+        productsContainer.insertAdjacentHTML('beforeend', productHTML);
     });
-
-
 }
-
